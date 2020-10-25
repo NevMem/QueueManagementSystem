9141ed79594624137f2d1055dee97912a4a3f07c
NevMem <memlolkek@gmail.com>
2020-10-25 15:50:10 +0300

    Simple navigation start setup

--- android/app/src/main/java/com/nevmem/qms/MainActivity.kt
+++ android/app/src/main/java/com/nevmem/qms/MainActivity.kt
@@ -2,10 +2,16 @@ package com.nevmem.qms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.navigationHost)
+        navView.setupWithNavController(navController)
     }
 }
