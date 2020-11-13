28562c0190c9cbdc0b806876add9ebc5f4fbb906
NevMem <memlolkek@gmail.com>
2020-11-13 18:09:25 +0300

    Created StatusFragment

---
+++ android/app/src/main/java/com/nevmem/qms/fragments/status/StatusFragment.kt
@@ -0,0 +1,14 @@
+package com.nevmem.qms.fragments.status
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.nevmem.qms.R
+
+class StatusFragment : Fragment(R.layout.fragment_queue_status) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
