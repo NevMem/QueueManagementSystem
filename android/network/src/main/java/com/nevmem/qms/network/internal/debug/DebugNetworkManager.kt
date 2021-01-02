3cfefd02d9e758dcd8dba1124f0deb70c6f99729
NevMem <memlolkek@gmail.com>
2021-01-02 17:58:32 +0300

    Small improvements for DebugNetworkManager

--- android/network/src/main/java/com/nevmem/qms/network/internal/debug/DebugNetworkManager.kt
+++ android/network/src/main/java/com/nevmem/qms/network/internal/debug/DebugNetworkManager.kt
@@ -1,13 +1,16 @@
 package com.nevmem.qms.network.internal.debug
 
+import com.google.gson.Gson
 import com.nevmem.qms.QueueDescriptionProto
 import com.nevmem.qms.network.NetworkManager
 import com.nevmem.qms.network.exceptions.BodyNotPresentException
+import com.nevmem.qms.network.exceptions.UnusualResponseCodeException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
@@ -18,15 +21,16 @@ internal class DebugNetworkManager : NetworkManager {
 
     data class QueueDescription(var name: String, var description: String, var imageUrl: String?)
 
+    private val gson = Gson()
+
     interface NetworkService {
         @Headers("Accept:application/json")
         @GET("/simple/{id}")
-        fun fetchDataByInvite(@Path("id") invite: String): Call<QueueDescription>
+        fun fetchDataByInvite(@Path("id") invite: String): Call<String>
     }
 
     private val retrofit = Retrofit.Builder()
-        // .addConverterFactory(ProtoConverterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create())
+        .addConverterFactory(ScalarsConverterFactory.create())
         .baseUrl("https://nevmem.com")
         .build()
 
@@ -36,24 +40,32 @@ internal class DebugNetworkManager : NetworkManager {
         backendService = retrofit.create(NetworkService::class.java)
     }
 
+    data class Description(var name: String? = null, var description: String? = null, var imageUrl: String? = null)
+
     override suspend fun fetchDataForInvite(invite: String): QueueDescriptionProto.QueueDescription = suspendCoroutine { continuation ->
         GlobalScope.launch(Dispatchers.IO) {
-            backendService.fetchDataByInvite(invite).enqueue(object : Callback<QueueDescription> {
-                override fun onFailure(call: Call<QueueDescription>, t: Throwable) {
+            backendService.fetchDataByInvite(invite).enqueue(object : Callback<String> {
+                override fun onFailure(call: Call<String>, t: Throwable) {
                     continuation.resumeWith(Result.failure(t))
                 }
 
                 override fun onResponse(
-                    call: Call<QueueDescription>,
-                    response: Response<QueueDescription>
+                    call: Call<String>,
+                    response: Response<String>
                 ) {
                     val body = response.body()
-                    if (response.isSuccessful && body != null) {
+                    if (response.code() != 200) {
+                        continuation.resumeWith(Result.failure(UnusualResponseCodeException(response.code())))
+                    } else if (response.isSuccessful && body != null) {
+                        val result: Description = gson.fromJson(body, Description::class.java)
                         val builder = QueueDescriptionProto.QueueDescription.newBuilder()
-                            .setName(body.name)
-                            .setDescription(body.description)
-                        body.imageUrl?.let { builder.setImageUrl(it) }
-                        continuation.resume(builder.build())
+                        result.apply {
+                            name?.let { builder.setName(it) }
+                            description?.let { builder.setDescription(it) }
+                            imageUrl?.let { builder.setImageUrl(it) }
+                        }
+                        val res = builder.build()
+                        continuation.resumeWith(Result.success(res))
                     } else {
                         continuation.resumeWith(Result.failure(BodyNotPresentException()))
                     }
