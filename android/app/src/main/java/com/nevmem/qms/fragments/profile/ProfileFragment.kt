7524a44c80155dbc9e893a0ddac70fb47b6502f2
NevMem <memlolkek@gmail.com>
2021-01-12 21:56:53 +0300

    Some moves of factories

--- android/app/src/main/java/com/nevmem/qms/fragments/profile/ProfileFragment.kt
+++ android/app/src/main/java/com/nevmem/qms/fragments/profile/ProfileFragment.kt
@@ -1,21 +1,17 @@
 package com.nevmem.qms.fragments.profile
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.view.doOnAttach
 import androidx.core.view.doOnDetach
 import androidx.core.view.isVisible
-import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.nevmem.qms.R
 import com.nevmem.qms.features.FeatureManager
 import com.nevmem.qms.inflate
@@ -26,11 +22,7 @@ import com.nevmem.qms.recycler.RVItemFactory
 import com.nevmem.qms.utils.ifDebug
 import com.nevmem.qms.utils.livedata.mergeLatest
 import kotlinx.android.synthetic.main.fragment_profile.*
-import kotlinx.android.synthetic.main.profile_avatar.view.*
-import kotlinx.android.synthetic.main.profile_email.view.*
 import kotlinx.android.synthetic.main.profile_header.view.*
-import kotlinx.android.synthetic.main.profile_lastname.view.*
-import kotlinx.android.synthetic.main.profile_name.view.*
 import kotlinx.android.synthetic.main.profile_rating.view.*
 import kotlinx.android.synthetic.main.profile_visited.view.*
 import org.koin.android.ext.android.inject
@@ -60,58 +52,6 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
                 ProfileVisitedFactory(),
                 HeaderFactory())
         })
-    }
-
-    private class ProfileAvatarFactory(private val context: Context) : RVItemFactory {
-        private inner class Holder(view: View) : RVHolder(view) {
-            override fun onBind(item: RVItem) {
-                item as ProfileFragmentViewModel.ProfileAvatar
-                Glide.with(context)
-                    .load(item.avatarUrl)
-                    .placeholder(R.drawable.icon_profile)
-                    .apply(RequestOptions().circleCrop())
-                    .into(itemView.avatarImage)
-            }
-        }
-        override fun isAppropriateType(item: RVItem): Boolean = item is ProfileFragmentViewModel.ProfileAvatar
-        override fun createHolder(root: ViewGroup): RVHolder
-            = Holder(LayoutInflater.from(context).inflate(R.layout.profile_avatar, root, false))
-    }
-
-    private class ProfileNameFactory(private val context: Context) : RVItemFactory {
-        private inner class Holder(view: View) : RVHolder(view) {
-            override fun onBind(item: RVItem) {
-                item as ProfileFragmentViewModel.ProfileName
-                itemView.profileName.text = item.name
-            }
-        }
-        override fun isAppropriateType(item: RVItem): Boolean = item is ProfileFragmentViewModel.ProfileName
-        override fun createHolder(root: ViewGroup): RVHolder
-            = Holder(LayoutInflater.from(context).inflate(R.layout.profile_name, root, false))
-    }
-
-    private class ProfileLastNameFactory(private val context: Context) : RVItemFactory {
-        private inner class Holder(view: View) : RVHolder(view) {
-            override fun onBind(item: RVItem) {
-                item as ProfileFragmentViewModel.ProfileLastName
-                itemView.profileLastName.text = item.lastName
-            }
-        }
-        override fun isAppropriateType(item: RVItem): Boolean = item is ProfileFragmentViewModel.ProfileLastName
-        override fun createHolder(root: ViewGroup): RVHolder
-            = Holder(LayoutInflater.from(context).inflate(R.layout.profile_lastname, root, false))
-    }
-
-    private class ProfileEmailFactory(private val context: Context) : RVItemFactory {
-        private inner class Holder(view: View) : RVHolder(view) {
-            override fun onBind(item: RVItem) {
-                item as ProfileFragmentViewModel.ProfileEmail
-                itemView.profileEmail.text = item.email
-            }
-        }
-        override fun isAppropriateType(item: RVItem): Boolean = item is ProfileFragmentViewModel.ProfileEmail
-        override fun createHolder(root: ViewGroup): RVHolder
-            = Holder(LayoutInflater.from(context).inflate(R.layout.profile_email, root, false))
     }
 
     private inner class ProfileRatingFactory : RVItemFactory {
