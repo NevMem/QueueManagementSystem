0ea9cbd310f6bdaf9eeed70e00fc1ce02274b039
NevMem <memlolkek@gmail.com>
2021-01-13 20:30:53 +0300

    Rounded corners + some content

--- android/scanner/src/main/java/com/nevmem/qms/scanner/QRScannerFragment.kt
+++ android/scanner/src/main/java/com/nevmem/qms/scanner/QRScannerFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.button.MaterialButton
 
 class QRScannerFragment : BottomSheetDialogFragment() {
     override fun onCreateView(
@@ -18,6 +21,17 @@ class QRScannerFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        view.findViewById<AppCompatImageView>(R.id.fragmentIcon).apply {
+            setImageResource(R.drawable.icon_camera)
+        }
+
+        view.findViewById<AppCompatTextView>(R.id.fragmentText).apply {
+            setText(R.string.ask_for_permissions)
+        }
+
+        view.findViewById<MaterialButton>(R.id.actionButton).apply {
+            setText(R.string.action_button_grant)
+        }
     }
 
     companion object {
