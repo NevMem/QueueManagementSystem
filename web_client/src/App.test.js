a90da825f76c4c52f564e51eed5ae231faf5259d
NevMem <memlolkek@gmail.com>
2021-01-17 16:05:18 +0300

    Test fix

--- web_client/src/App.test.js
+++ web_client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders login form', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Login or register to moderate your queues/i);
   expect(linkElement).toBeInTheDocument();
 });
