a8e14adfac7da833b457ab8c96b6e9755c7e6d29
pufit <pufit@yandex.ru>
2021-01-18 21:18:56 +0300

    Middleware (part 1)

--- server/app/main.py
+++ server/app/main.py
@@ -1,40 +1,32 @@
 from aiohttp import web
 from itsdangerous.serializer import Serializer
-from Crypto.Hash import SHA256
 
 from server.app.config import Config
 from server.app import model
 from server.app.utils.db_utils import session_scope
+from server.app.utils import sha_hash
+from server.app import middleware
 from proto import user_pb2
 
 routes = web.RouteTableDef()
 serializer = Serializer(Config.SECRET_KEY)
 
 
-def sha_hash(s: str) -> str:
-    return SHA256.new(s.encode()).hexdigest()
-
-
-@routes.get('/ping')
-async def ping(request: web.Request):
-    return web.Response()
-
-
 @routes.post('/auth')
-async def auth(request: web.Request):
-    user_identity = user_pb2.UserIdentity.ParseFromString(await request.read())
-
+@middleware.default
+async def auth(request: user_pb2.UserIdentity, _):
     async with session_scope() as session:
         if user_id := await (
             session.query(model.User.id)
             .filter(
-                model.User.email == user_identity.email,
-                model.User.password == sha_hash(user_identity.password)
+                model.User.email == request.email,
+                model.User.password == sha_hash(request.password)
             )
         ):
             token = serializer.dumps({'id': user_id})
 
-    # todo: response, Proto/Json serializer
+    # todo: response
+
 
 def main():
     app = web.Application()
---
+++ server/app/middleware.py
@@ -0,0 +1,91 @@
+import functools
+import typing as tp
+from aiohttp import web
+
+from google.protobuf.message import Message
+from google.protobuf.json_format import Parse, ParseDict
+
+
+class Context:
+
+    def __getattribute__(self, item):
+        if item not in super().__getattribute__('__dict__'):
+            return None
+        return super().__getattribute__(item)
+
+
+class AbstractMiddleware:
+
+    async def process_request(self, request, context: Context):
+        return request, context
+
+    async def process_response(self, response, context: Context):
+        return response, context
+
+    async def process_finally(self):
+        pass
+
+
+class ProtobufMiddleware(AbstractMiddleware):
+
+    # noinspection PyBroadException
+    async def process_request(self, request: web.Request, context: Context) -> Message:
+        data = await request.read()
+        try:
+            request = context.request_type().ParseFromString(data)
+        except:
+            request = Parse(data, context.request_type(), ignore_unknown_fields=True)
+            context.is_rest = True
+
+        return request
+
+    async def process_response(self, response: Message, context: Context) -> web.Response:
+        pass  # TODO: response
+
+
+class MiddlewareApplier:
+
+    def __init__(self, *middlewares: AbstractMiddleware):
+        self.middlewares: tp.Tuple[AbstractMiddleware] = middlewares
+
+    def __call__(self, func: tp.Callable):
+
+        @functools.wraps(func)
+        async def _wrapped(request, context: tp.Optional[Context] = None):
+            if context is None:
+                context = Context()
+
+            # noinspection PyUnresolvedReferences
+            annotations = func.__annotations__
+
+            try:
+                context.response_type = annotations.pop('returns')
+            except KeyError:
+                raise ValueError(f'Response type for function {func} not specified')
+
+            try:
+                context.request_type = next(iter(annotations.values()))
+            except StopIteration:
+                raise ValueError(f'Request type for function {func} not specified')
+
+            middleware_processed = 0
+            try:
+                for middleware in self.middlewares:
+                    middleware_processed += 1
+                    request = await middleware.process_request(request, context)
+
+                response = await func(request, context)
+
+                for middleware in reversed(self.middlewares):
+                    response = await middleware.process_response(response, context)
+            finally:
+
+                for middleware in reversed(self.middlewares):
+                    await middleware.process_finally()
+
+            return response
+
+        return _wrapped
+
+
+default = MiddlewareApplier(ProtobufMiddleware(), )
