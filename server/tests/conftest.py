62355debaceeab36aabd4c188d3ef143111b28c4
nikitonsky <nikita@niitonsky.tk>
2021-01-22 18:52:02 +0300

    fix_conftest

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -3,7 +3,9 @@ import subprocess
 import typing as tp
 import time
 import requests
-
+from server.app.main import app
+from starlette.testclient import TestClient
+import os
 
 @pytest.fixture(scope='function')
 def db():
@@ -16,7 +18,7 @@ def db():
 
 
 @pytest.fixture(scope='function')
-def server(db: Database):
+def server(db: 'Database'):
     server = Server(db.connection_string)
     try:
         server.start()
@@ -56,29 +58,16 @@ class Process:
         self.process = None
 
 
-class Server(Process):
-    def __init__(self, db):
-        super(Server).__init__(["uvicorn", "server.app.main:app", "--reload"], env={'DB_URL': db})
-
-    def http_get(self, path, auth=None, **kwargs):
-        headers = {}
-        response = requests.get(
-            'http://localhost:8000' + path,
-            params=dict((k, v) for k, v in kwargs.items() if v is not None),
-            headers=headers,
-            timeout=60)
-        return response
-
-    def ping(self):
-        return self.http_get('/ping').status_code
-
-    def healthcheck(self):
-        return self.ping() == 200
+class Server(TestClient):
+    def __init__(self, db_url):
+        os.environ['DB_URL'] = db_url
+        super(Server, self).__init__(app)
 
 
 class Database(Process):
     def __init__(self):
         super(Database, self).__init__(['postgres', '-D', '/tmp/postgre'])
+
     @property
     def connection_string(self) -> str:
         return 'abrvalg'
