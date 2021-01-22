fc6d5d1adb243533cc6faead7bfebe1447cbca56
nikitonsky <nikita@niitonsky.tk>
2021-01-22 19:16:36 +0300

    fix

--- server/tests/helpers/prepare_env.sh
+++ server/tests/helpers/prepare_env.sh
@@ -6,7 +6,5 @@ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt update
 sudo apt -y install postgresql-12 python3.9 python3-pip python3-pytest
-pip3 install setuptools wheel
-pip3 install -r ./server/app/requirenments.txt
-
-pip3 install pytest
+python3.9 -m pip install setuptools wheel pytest
+python3.9 -m pip install -r ./server/app/requirenments.txt
