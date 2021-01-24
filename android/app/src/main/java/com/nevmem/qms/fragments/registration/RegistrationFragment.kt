0dcab6c31ce383ddcc828bef7c339757ae706ccb
NevMem <memlolkek@gmail.com>
2021-01-24 17:52:00 +0300

    Some layouts init + some action for registration page

---
+++ android/app/src/main/java/com/nevmem/qms/fragments/registration/RegistrationFragment.kt
@@ -0,0 +1,8 @@
+package com.nevmem.qms.fragments.registration
+
+import androidx.fragment.app.Fragment
+import com.nevmem.qms.R
+
+class RegistrationFragment : Fragment(R.layout.fragment_registration) {
+
+}
