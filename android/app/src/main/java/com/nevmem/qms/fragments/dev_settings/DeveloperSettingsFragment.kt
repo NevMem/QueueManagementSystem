697900d2e31a35f09f484dd6c33124fd75a6e6ad
NevMem <memlolkek@gmail.com>
2021-02-08 16:49:42 +0300

    Added developer settings fragment

---
+++ android/app/src/main/java/com/nevmem/qms/fragments/dev_settings/DeveloperSettingsFragment.kt
@@ -0,0 +1,12 @@
+package com.nevmem.qms.fragments.dev_settings
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.nevmem.qms.R
+
+class DeveloperSettingsFragment : Fragment(R.layout.fragment_developer_settings) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
