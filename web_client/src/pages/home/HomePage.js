83371b6561b7ffbdab55fcece90cb4a8b6924730
NevMem <memlolkek@gmail.com>
2021-02-23 00:18:30 +0300

    Designing HomePage

--- web_client/src/pages/home/HomePage.js
+++ web_client/src/pages/home/HomePage.js
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from 'react'
 import Header from '../../components/header/Header'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import AddButton from '../../components/buttons/add_button/AddButton'
 
 export default class HomePage extends Component {
     constructor(prps) {
@@ -11,6 +14,16 @@ export default class HomePage extends Component {
         return (
             <Fragment>
                 <Header />
+                <Grid container justify='center'>
+                    <Grid item xs={10}>
+                        <Grid container justify='space-evenly' style={{marginTop: '16px'}}>
+                            <Typography style={{color: '#a0a0a0', fontSize: '26px'}} variant='body2'>
+                                Ваши организации
+                            </Typography>
+                            <AddButton isPrimaryButton={true} text='Новая организация' />
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Fragment>
         )
     }
