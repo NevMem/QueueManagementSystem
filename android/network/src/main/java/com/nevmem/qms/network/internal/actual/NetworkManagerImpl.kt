a48b6ff0064b07113dbf25265d89338ca29d45d7
NevMem <memlolkek@gmail.com>
2021-02-22 17:07:00 +0300

    Update for id

--- android/network/src/main/java/com/nevmem/qms/network/internal/actual/NetworkManagerImpl.kt
+++ android/network/src/main/java/com/nevmem/qms/network/internal/actual/NetworkManagerImpl.kt
@@ -37,7 +37,7 @@ internal class NetworkManagerImpl(
     }
 
     interface FeaturesService {
-        @GET("/config/1674cd24-ee20-41be-aea8-ded02649e8c3")
+        @GET("/config/56cf679c-beb3-4f64-ac63-b8d3697b9cc2")
         fun loadFeatures(): Call<Map<String, String>>
     }
 
