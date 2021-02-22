1632721800eb50c113f726d14f4dc5fca0ac9b8b
NevMem <memlolkek@gmail.com>
2021-02-22 18:21:03 +0300

    Move to status page after joining

--- android/app/src/main/java/com/nevmem/qms/fragments/join/step/DoJoinStep.kt
+++ android/app/src/main/java/com/nevmem/qms/fragments/join/step/DoJoinStep.kt
@@ -1,13 +1,22 @@
 package com.nevmem.qms.fragments.join.step
 
+import android.os.Bundle
+import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.nevmem.qms.R
+import com.nevmem.qms.fragments.join.JoinFragmentDirections
 import com.nevmem.qms.fragments.join.JoinStep
 import com.nevmem.qms.fragments.join.JoinUsecase
 import com.nevmem.qms.status.FetchStatus
+import com.nevmem.qms.status.JoinStatus
+import com.nevmem.qms.status.StatusProvider
+import com.nevmem.qms.toast.manager.ShowToastManager
+import com.nevmem.qms.utils.runOnUi
 import kotlinx.android.synthetic.main.fragment_step_do_join.*
+import kotlinx.coroutines.flow.collect
 import org.koin.android.ext.android.inject
 
 class DoJoinStep : JoinStep {
@@ -17,9 +26,11 @@ class DoJoinStep : JoinStep {
         class DoJoinFragment : Fragment(R.layout.fragment_step_do_join) {
 
             private val usecase: JoinUsecase by inject()
+            private val statusProvider: StatusProvider by inject()
+            private val showToastManager: ShowToastManager by inject()
 
-            override fun onResume() {
-                super.onResume()
+            override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+                super.onViewCreated(view, savedInstanceState)
 
                 usecase.fetchStatus?.let { status ->
                     if (status is FetchStatus.Success) {
@@ -34,6 +45,26 @@ class DoJoinStep : JoinStep {
 
                         inviteDescription.text = status.payload.description
                         inviteName.text = status.payload.name
+
+                        joinButton.isEnabled = true
+                        joinButton.setOnClickListener {
+                            joinButton.isEnabled = false
+                            runOnUi {
+                                statusProvider.join(status.payload).collect {
+                                    when (it) {
+                                        is JoinStatus.Pending -> {}
+                                        is JoinStatus.Success -> {
+                                            joinButton.isEnabled = true
+                                            findNavController().navigate(JoinFragmentDirections.moveToStatusPage())
+                                        }
+                                        is JoinStatus.Error -> {
+                                            joinButton.isEnabled = true
+                                            showToastManager.error(it.message)
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
--- android/app/src/main/java/com/nevmem/qms/utils/utils.kt
+++ android/app/src/main/java/com/nevmem/qms/utils/utils.kt
@@ -1,9 +1,18 @@
 package com.nevmem.qms.utils
 
 import com.nevmem.qms.BuildConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 inline fun ifDebug(crossinline block: () -> Unit) {
     if (BuildConfig.DEBUG) {
+        block()
+    }
+}
+
+inline fun runOnUi(crossinline block: suspend () -> Unit) {
+    GlobalScope.launch(Dispatchers.Main) {
         block()
     }
 }
