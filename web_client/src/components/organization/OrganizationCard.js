aa02511297c1512b36593faf39ca7dcdea2eae33
NevMem <memlolkek@gmail.com>
2021-02-23 20:54:20 +0300

    Mock data in json

--- web_client/src/components/organization/OrganizationCard.js
+++ web_client/src/components/organization/OrganizationCard.js
@@ -3,14 +3,41 @@ import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import AddButton from '../../components/buttons/add_button/AddButton'
 
+const ServiceRow = ({ serviceData }) => {
+    return (
+        <Grid container style={{paddingLeft: '16px', marginTop: '16px'}}>
+            <Grid item xs={2}>
+                <Typography style={{color: '#a0a0a0', fontSize: '20px'}} variant='body2'>
+                    {serviceData.name}
+                </Typography>
+            </Grid>
+            <Grid item xs={4}>
+                <Typography style={{color: '#a0a0a0', fontSize: '16px', textAlign: 'right'}} variant='body2'>
+                    {serviceData.waiting}
+                </Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography style={{color: '#a0a0a0', fontSize: '16px', textAlign: 'right'}} variant='body2'>
+                    {serviceData.avgServiceTime}
+                </Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <Typography style={{color: '#a0a0a0', fontSize: '20px', textAlign: 'right'}} variant='body2'>
+                    {serviceData.rating}
+                </Typography>
+            </Grid>
+        </Grid>
+    )
+}
 
-export default function OrganizationCard({...props}) {
+export default function OrganizationCard({organizationData, ...props}) {
+    const { name, services } = organizationData
     return (
         <div className = 'organizationCard' {...props}>
             <Grid container justify='space-between'>
                 <Grid item xs={4}>
                     <Typography style={{color: '#c0c0c0', fontSize: '30px'}} variant='body2'>
-                        Аптека на тверской
+                        {name}
                     </Typography>
                 </Grid>
                 <Grid item xs={2}>
@@ -24,20 +51,9 @@ export default function OrganizationCard({...props}) {
                     </Typography>
                 </Grid>
             </Grid>
-            <Grid container style={{paddingLeft: '16px'}}>
-                <Grid item xs={2}>
-                    <Typography style={{color: '#a0a0a0', fontSize: '20px'}} variant='body2'>Кофе</Typography>
-                </Grid>
-                <Grid item xs={4}>
-                    <Typography style={{color: '#a0a0a0', fontSize: '16px', textAlign: 'right'}} variant='body2'>10 клиентов ожидают</Typography>
-                </Grid>
-                <Grid item xs={3}>
-                    <Typography style={{color: '#a0a0a0', fontSize: '16px', textAlign: 'right'}} variant='body2'>~1:30 мин/чел</Typography>
-                </Grid>
-                <Grid item xs={3}>
-                    <Typography style={{color: '#a0a0a0', fontSize: '20px', textAlign: 'right'}} variant='body2'>4.0</Typography>
-                </Grid>
-            </Grid>
+            { services.map(elem => {
+                return <ServiceRow key={elem.id} serviceData={elem} />
+            }) }
         </div>
     )
 }
--- web_client/src/pages/home/HomePage.js
+++ web_client/src/pages/home/HomePage.js
@@ -12,6 +12,62 @@ export default class HomePage extends Component {
     }
     
     render() {
+
+        const organizationsData = [
+            {
+                id: 'some-id-1',
+                name: 'Аптека на Тверской',
+                services: [
+                    {
+                        id: 'service-1-1',
+                        name: 'Кафе',
+                        avgServiceTime: '~1:30 мин/чел',
+                        waiting: '10 человек ожидают',
+                        rating: '4.0',
+                        isWorking: true
+                    },
+                    {
+                        id: 'service-1-2',
+                        name: 'Таблетки',
+                        avgServiceTime: '~0:40 мин/чел',
+                        waiting: '2 человек ожидают',
+                        rating: '4.9',
+                        isWorking: true
+                    },
+                ]
+            },
+            {
+                id: 'some-id-2',
+                name: 'Аптека на Баумана (Казань)',
+                services: [
+                    {
+                        id: 'service-2-1',
+                        name: 'Кофепоинт',
+                        avgServiceTime: '~2:30 мин/чел',
+                        waiting: '5 человек ожидают',
+                        rating: '4.3',
+                        isWorking: true
+                    },
+                    {
+                        id: 'service-2-2',
+                        name: 'Таблетки',
+                        avgServiceTime: '~0:40 мин/чел',
+                        waiting: '0 человек ожидают',
+                        rating: '3.0',
+                        isWorking: true
+                    },
+                    {
+                        id: 'service-2-3',
+                        name: 'Варенье',
+                        avgServiceTime: '~0:20 мин/чел',
+                        waiting: '20 человек ожидают',
+                        rating: '4.99',
+                        isWorking: true
+                    },
+                ]
+            }
+        ]
+
         return (
             <Fragment>
                 <Header />
@@ -23,8 +79,11 @@ export default class HomePage extends Component {
                             </Typography>
                             <AddButton isPrimaryButton={true} text='Новая организация' />
                         </Grid>
-                        <OrganizationCard style={{marginTop: '20px'}} />
-                        <OrganizationCard style={{marginTop: '20px'}} />
+                        {/* <OrganizationCard style={{marginTop: '20px'}}  /> */}
+                        {/* <OrganizationCard style={{marginTop: '20px'}} /> */}
+                        { organizationsData.map((elem) => {
+                            return <OrganizationCard key={elem.id} organizationData={elem} style={{marginTop: '20px'}}  />
+                        }) }
                     </Grid>
                 </Grid>
             </Fragment>
