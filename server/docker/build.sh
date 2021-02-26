0b57f2e51d56818de82da03c92cc5f7e5ecebc63
nikitonsky <nikita@niitonsky.tk>
2021-02-26 18:43:38 +0300

    Init postcommit docker build

--- server/docker/build.sh
+++ server/docker/build.sh
@@ -6,6 +6,6 @@ mkdir -p tmp/usr/lib/qms/server/
 cp -r ../app tmp/usr/lib/qms/server
 cp -r ../../proto tmp/usr/lib/qms/
 
-docker build . --network=host
+docker build . --network=host -t cr.yandex/crp59su1rb652ia7ojk8/init:$(git rev-parse HEAD)
 
 rm -rf tmp
