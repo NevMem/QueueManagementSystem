5f5faedb695618ba404d83a8407c4e1776aa2539
NevMem <memlolkek@gmail.com>
2021-03-08 15:24:56 +0300

    Added unauthorized wrapper

--- web_client/src/api/wrappers.js
+++ web_client/src/api/wrappers.js
@@ -1,7 +1,21 @@
-export const logWrapper = (promise) => {
+const logWrapper = (promise) => {
     return promise.then(data => { console.log('Response: ' + JSON.stringify(data)); return data; })
+}
+
+const unauthorizedWrapper = (promise) => {
+    return promise.then(data => {
+        console.log('Unauhtorized')
+        if (data.status == 403) {
+            window.location.reload()
+        }
+        return data
+    })
 }
 
 export const defaultRequestWrapper = (promise) => {
     return logWrapper(promise)
+}
+
+export const authorizedRequestWrapper = (promise) => {
+    return unauthorizedWrapper(defaultRequestWrapper(promise))
 }
