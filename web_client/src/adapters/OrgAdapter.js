4d74dad8c7a3368b21972a2d20991bb8586fbe77
NevMem <memlolkek@gmail.com>
2021-03-10 21:44:37 +0300

    Decomposition

--- web_client/src/adapters/OrgAdapter.js
+++ web_client/src/adapters/OrgAdapter.js
@@ -1,59 +1,6 @@
 import { loadOrganizationList, createOrganization } from '../api/orgApi'
 import authAdapter from './AuthAdapter'
-
-const parseService = (json) => {
-    if (!json.info || !json.info.id) {
-        return undefined
-    }
-    const service = {
-        id: json.info.id,
-        name: json.info.name
-    }
-    return service
-}
-
-const parseServices = (json) => {
-    if (!json) {
-        return []
-    }
-    const services = []
-    for (const serviceItem of json) {
-        const parsed = parseService(serviceItem)
-        if (parsed) {
-            services.push(parsed)
-        }
-    }
-    return services
-}
-
-const parseOrganization = (json) => {
-    if (!json.info || !json.info.id) {
-        return undefined
-    }
-    const organization = {
-        id: json.info.id,
-        name: json.info.name,
-        services: parseServices(json.services)
-    }
-    return organization
-}
-
-const parseOrganizations = (json) => {
-    if (!json.organisations) {
-        return []
-    }
-
-    const organizations = []
-
-    for (const orgItem of json.organisations) {
-        const parsed = parseOrganization(orgItem)
-        if (parsed) {
-            organizations.push(parsed)
-        }
-    }
-
-    return organizations
-}
+import parseOrganizations from '../parsers/parseOrganizations'
 
 class OrgAdapter {
     constructor() {
