91a0b9f62f613eebbff6b287040a2dbbe797655d
NevMem <memlolkek@gmail.com>
2021-03-17 00:46:41 +0300

    Added countTokens handle

--- push/core/src/main/kotlin/com/nevmem/qms/push/controllers/InfoController.kt
+++ push/core/src/main/kotlin/com/nevmem/qms/push/controllers/InfoController.kt
@@ -1,9 +1,18 @@
 package com.nevmem.qms.push.controllers
 
+import com.nevmem.qms.push.service.TokenStorageService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @CrossOrigin("*")
-@RestController("/api/info")
-class InfoController {
+@RestController
+@RequestMapping("/api/info")
+class InfoController @Autowired constructor(
+    private val tokenStorageService: TokenStorageService
+) {
+
+    @RequestMapping("/countTokens")
+    fun countTokens(): Int = tokenStorageService.fetchTokens().size
 }
