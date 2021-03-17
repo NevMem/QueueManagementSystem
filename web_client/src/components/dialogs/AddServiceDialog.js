a1f59a65710d889f49b34cbc0b4a1a353ef6af3e
NevMem <memlolkek@gmail.com>
2021-03-17 23:20:27 +0300

    Start working on checklists

--- web_client/src/components/dialogs/AddServiceDialog.js
+++ web_client/src/components/dialogs/AddServiceDialog.js
@@ -1,13 +1,21 @@
+import { Fragment, useState } from 'react'
 import { green } from '@material-ui/core/colors'
-import { useState } from 'react'
+import { makeAutoObservable } from 'mobx'
+import { observer } from 'mobx-react'
 import { withStyles } from '@material-ui/core/styles'
+import AddIcon from '@material-ui/icons/Add'
 import Button from '@material-ui/core/Button'
+import DeleteIcon from '@material-ui/icons/Delete'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import Grid from '@material-ui/core/Grid'
+import IconButton from '@material-ui/core/IconButton'
+import InputAdornment from '@material-ui/core/InputAdornment'
 import localizedString from '../../localization/localizedString'
 import MuiDialog from '@material-ui/core/Dialog'
 import orgAdapter from '../../adapters/OrgAdapter'
+import OutlinedInput from '@material-ui/core/OutlinedInput'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import useInput from '../../utils/useInput'
@@ -35,24 +43,81 @@ const Dialog = withStyles({
 class Checklist {
     constructor() {
         this.list = []
+        this.id = 0
+        makeAutoObservable(this)
     }
 
     getList() {
         return this.list
     }
 
-    addList(title) {
-        this.list.push(title)
+    addItem(title) {
+        const newList = [...this.list]
+        newList.push({
+            id: this.id,
+            name: title
+        })
+        this.list = newList
+        this.id += 1
     }
 
-    removeList(title) {
-        const index = this.list.indexOf(title)
+    removeItem(item) {
+        const index = this.list.indexOf(item)
         if (index < 0) {
             return
         }
         this.list.splice(index, 1)
     }
 }
+
+const ChecklistBlock = observer(({ checklist }) => {
+
+    const [value, setValue] = useState('')
+    const handleChange = (ev) => {
+        setValue(ev.target.value)
+    }
+    const addItem = () => {
+        checklist.addItem(value)
+        setValue('')
+    }
+    const handleDelete = (item) => {
+        checklist.removeItem(item)
+    }
+
+    return (
+        <Fragment>
+            { checklist.getList().map((elem) => {
+                return (
+                    <Grid container key={elem.id} justify='space-between'>
+                        <Typography style={{lineHeight: '48px'}}>{elem.name}</Typography>
+                        <IconButton
+                            aria-label='delete checklist item'
+                            onClick={handleDelete.bind(elem, elem)}>
+                                <DeleteIcon />
+                        </IconButton>
+                    </Grid>
+                )
+            }) }
+            <Grid container>
+                <OutlinedInput
+                    margin='dense'
+                    value={value}
+                    style={{width: '100%'}}
+                    endAdornment={
+                        <InputAdornment position='end'>
+                            <IconButton
+                                aria-label="add item to checklist"
+                                onClick={addItem}
+                                edge="end">
+                                <AddIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    onChange={handleChange} />
+            </Grid>
+        </Fragment>
+    )
+})
 
 export default function AddServiceDialog({ organization, open, onClose, ...rest }) {
 
@@ -75,6 +140,8 @@ export default function AddServiceDialog({ organization, open, onClose, ...rest 
             })
     }
 
+    const checklist = new Checklist()
+
     return (
         <Dialog
                 {...rest}
@@ -93,7 +160,7 @@ export default function AddServiceDialog({ organization, open, onClose, ...rest 
                     label={localizedString('add_new_service_dialog_label')}
                     {...bindServiceName} />
 
-
+                <ChecklistBlock checklist={checklist} />
 
             </DialogContent>
             <DialogActions>
