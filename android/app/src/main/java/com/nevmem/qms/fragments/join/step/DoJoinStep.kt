1556cdb61c3d80faddf3cb55d8d9dcd71416c2aa
NevMem <memlolkek@gmail.com>
2021-03-21 16:25:24 +0300

    Remove unused

--- android/app/src/main/java/com/nevmem/qms/fragments/join/step/DoJoinStep.kt
+++ android/app/src/main/java/com/nevmem/qms/fragments/join/step/DoJoinStep.kt
@@ -6,7 +6,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
