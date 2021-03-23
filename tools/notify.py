54701b23b94de09f675047b2f99391cc41f87926
NevMem <memlolkek@gmail.com>
2021-03-23 22:45:57 +0300

    Try make better

--- tools/notify.py
+++ tools/notify.py
@@ -12,7 +12,7 @@ def send_message(bot_token, chat_id, message):
     assert(response.status_code == 200)
 
 
-def create_message(notify_success):
+def create_message(notify_success, notify_flaky):
     if not Path('run_results.json').exists():
         return textwrap.dedent("""
             ‼️‼️‼️ Это автоматическое сообщение из автотестов
@@ -23,7 +23,20 @@ def create_message(notify_success):
         """)
     with open('run_results.json', 'r') as inp:
         data = json.loads(inp.read())
-        if len(data['fails']) == 0:
+
+        fails_count = 0
+        flaky_fails_count = 0
+        for fail in data['fails']:
+            for report in data['fails']:
+                if report['name'] == fail:
+                    fails_count += report['is_flaky'] == False
+                    flaky_fails_count += report['is_flaky'] == True
+
+        actual_fails = fails_count
+        if notify_flaky:
+            actual_fails += flaky_fails_count
+
+        if actual_fails == 0:
             if notify_success:
                 return textwrap.dedent("""
                     ✅ Все сценарии отработали успешно!
@@ -39,6 +52,9 @@ def create_message(notify_success):
         for fail in data['fails']:
             text += fail + '\n'
 
+        if notify_flaky:
+            text += "Из них flaky тестов " + str(flaky_fails_count) + '\n'
+
         text += "\nСработавшие сценарии:\n"
         for success in data['success']:
             text += success + '\n'
@@ -50,10 +66,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--token', help='Bot token')
     parser.add_argument('--chat_ids', help='Chat ids')
-    parser.add_argument('--notify_success', help='Should we notify about fully successfull run', default=False, type=bool)
+    parser.add_argument(
+        '--notify_success',
+        help='Should we notify about fully successfull run',
+        default=False,
+        type=bool)
+    parser.add_argument(
+        '--notify_flaky',
+        help='Should we notify about flaky test fail',
+        default=False)
     args = parser.parse_args()
 
-    message = create_message(args.notify_success)
+    message = create_message(args.notify_success, args.notify_flaky)
     print(message)
 
     if message is not None:
