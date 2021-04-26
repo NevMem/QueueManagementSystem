61435c71d45d727a8869eb778a8af8aa350d46fa
NevMem <memlolkek@gmail.com>
2021-04-26 09:38:39 +0300

    put api key in shared code

--- android/qmskit/src/main/cpp/qmskit.cpp
+++ android/qmskit/src/main/cpp/qmskit.cpp
@@ -5,6 +5,11 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_nevmem_qms_qmskit_QMSKit_apiKey(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "API_KEY";
-    return env->NewStringUTF(hello.c_str());
+
+#define APP_API_KEY(X) \
+    static std::string API_KEY = X;
+#include "../../../../../shared/apikey/apikey.def"
+#undef APP_API_KEY
+
+    return env->NewStringUTF(API_KEY.c_str());
 }
