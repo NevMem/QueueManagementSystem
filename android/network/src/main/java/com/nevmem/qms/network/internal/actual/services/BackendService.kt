a02498a57c3cd665c51e90918c65ff970fd8a7bd
NevMem <memlolkek@gmail.com>
2021-05-04 21:07:56 +0300

    remove unused

--- android/network/src/main/java/com/nevmem/qms/network/internal/actual/services/BackendService.kt
+++ android/network/src/main/java/com/nevmem/qms/network/internal/actual/services/BackendService.kt
@@ -8,28 +8,8 @@ import retrofit2.http.POST
 
 data class UserIdentity(val email: String?, val password: String?)
 data class RegisterRequest(val name: String?, val surname: String?, val identity: UserIdentity?)
-data class User(val id: String?, val name: String?, val surname: String?, val email: String?)
-data class OrganizationInfo(
-    var id: String = "", var name: String = "", var address: String = "", var data: Map<String, String> = mapOf())
 data class ServiceInfo(
     var id: String = "", var name: String = "", var organizationId: String = "", var data: Map<String, String> = mapOf())
-data class Service(var info: ServiceInfo)
-data class Organization(var info: OrganizationInfo? = null, var services: List<Service> = listOf())
-enum class TicketState {
-    WAITING, PROCESSING, PROCESSED
-}
-data class Ticket(
-    var state: TicketState? = null,
-    var id: String = "",
-    var userId: String = "",
-    var serviceId: String = "",
-    var ticketId: String = "",
-    var organizationId: String = "",
-    var window: String = "")
-data class TicketInfo(
-    var ticket: Ticket? = null,
-    var remainingTime: Int?,
-    var peopleInFront: Int?)
 
 interface BackendService {
     @POST("/client/login")
@@ -40,7 +20,4 @@ interface BackendService {
 
     @POST("/client/enter_queue")
     fun join(@Header("session") session: String, @Body body: ServiceInfo): Call<Unit>
-
-    @POST("/client/get_current_queue_info")
-    fun currentTicketInfo(@Header("session") session: String): Call<TicketInfo>
 }
