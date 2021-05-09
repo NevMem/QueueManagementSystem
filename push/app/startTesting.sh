6583e2ce84878a702f17f8b7b83d4c7833bf97af
NevMem <memlolkek@gmail.com>
2021-05-09 18:45:24 +0300

    Some preparations for split

--- push/app/startTesting.sh
+++ push/app/startTesting.sh
@@ -1,3 +1,6 @@
-export PUSH_WORKING_DIR=$(pwd)
-./gradlew bootRun
+# should be executed from root of project
 
+export FIREBASE_CREDENTIALS=$(pwd)/push/secrets/firebase-push-secret.json
+cd push/app
+./gradlew bootRun
+cd ../../
