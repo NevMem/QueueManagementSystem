d57f072b4a6bc87eeb70eaba0bed052bd3150f83
nikitonsky <nikita@niitonsky.tk>
2021-05-18 19:24:56 +0300

    better

--- push/docker/build.sh
+++ push/docker/build.sh
@@ -3,10 +3,10 @@
 mkdir -p tmp || exit 1
 rm -rf tmp/* || exit 1
 mkdir -p tmp/usr/lib/qms/push/ || exit 1
-cd ../app
-./gradelew build
+cd ../app || exit 1
+./gradelew build || exit 1
 cp -r push/build/libs/push* ../docker/tmp/usr/lib/qms/push.jar || exit 1
-cd ../docker
+cd ../docker || exit 1
 
 docker build . --network=host -t cr.yandex/crp59su1rb652ia7ojk8/push:$(git rev-parse HEAD) || exit 1
 
