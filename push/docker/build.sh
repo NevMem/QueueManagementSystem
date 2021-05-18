359671ad851411012ba07bff6302d2a7a95eb186
nikitonsky <nikita@niitonsky.tk>
2021-05-18 19:22:44 +0300

    Fix

--- push/docker/build.sh
+++ push/docker/build.sh
@@ -3,8 +3,10 @@
 mkdir -p tmp || exit 1
 rm -rf tmp/* || exit 1
 mkdir -p tmp/usr/lib/qms/push/ || exit 1
-../app/gradelew build
-cp -r ../app/push/build/libs/push* tmp/usr/lib/qms/push.jar || exit 1
+cd ../app
+./gradelew build
+cp -r push/build/libs/push* ../docker/tmp/usr/lib/qms/push.jar || exit 1
+cd ../docker
 
 docker build . --network=host -t cr.yandex/crp59su1rb652ia7ojk8/push:$(git rev-parse HEAD) || exit 1
 
