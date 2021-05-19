c861b2b9c439c1ca6718e0ef52fc87edeff94eba
nikitonsky <naoskotskiy@edu.hse.ru>
2021-05-19 19:18:47 +0300

    fix (#173)
    Co-authored-by: nikitonsky <nikita@niitonsky.tk>

--- feedback/docker/build.sh
+++ feedback/docker/build.sh
@@ -5,7 +5,7 @@ rm -rf tmp/* || exit 1
 mkdir -p tmp/usr/lib/qms/feedback/ || exit 1
 cd ../app || exit 1
 ./gradlew build || exit 1
-cp -r push/build/libs/push* ../docker/tmp/usr/lib/qms/feedback.jar || exit 1
+cp -r push/build/libs/feedback* ../docker/tmp/usr/lib/qms/feedback.jar || exit 1
 cd ../docker || exit 1
 
 docker build . --network=host -t cr.yandex/crp59su1rb652ia7ojk8/feedback:$(git rev-parse HEAD) || exit 1
