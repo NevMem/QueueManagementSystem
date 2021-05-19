879d48f45a4cca23f2e9f581a397d394a4cd5c08
nikitonsky <nikita@niitonsky.tk>
2021-05-19 18:45:10 +0300

    Disable prometheus

--- push/app/push/src/main/kotlin/com/nevmem/qms/push/PushServer.kt
+++ push/app/push/src/main/kotlin/com/nevmem/qms/push/PushServer.kt
@@ -2,10 +2,8 @@ package com.nevmem.qms.push
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics
 
 @SpringBootApplication(scanBasePackages = ["com.nevmem.qms"])
-@EnablePrometheusMetrics
 class PushServer
 
 fun main(args: Array<String>) {
