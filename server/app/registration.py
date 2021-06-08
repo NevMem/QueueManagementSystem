2169aa45cd50ff58632e3d9f6b0a446c2dc46d15
nikitonsky <naoskotskiy@edu.hse.ru>
2021-06-08 20:03:03 +0300

    Email notification implementation (#181)
    * Email notification implementation
    
    * fix
    
    Co-authored-by: nikitonsky <nikita@niitonsky.tk>

---
+++ server/app/registration.py
@@ -0,0 +1,32 @@
+import aiosmtplib
+
+from server.app.config import Config
+from email.message import EmailMessage
+
+
+class MailManager:
+    client = None
+
+    @classmethod
+    async def send_confirmation_email(cls, destination: str, confirmation_id: str):
+        if cls.client is None:
+            if Config.MAIL_PASSWORD is None:
+                return
+            cls.client = aiosmtplib.SMTP(
+                hostname=Config.MAIL_HOST,
+                port=Config.MAIL_PORT,
+                username=Config.MAIL_USERNAME,
+                password=Config.MAIL_PASSWORD,
+                use_tls=True
+            )
+            await cls.client.connect()
+        message = EmailMessage()
+        message['From'] = Config.MAIL_USERNAME
+        message['To'] = destination
+        message['Subject'] = 'Confirm your registration in Queue Management Service'
+        message.set_content(
+            f'https://qms-back.nikitonsky.tk/confirm_registration?confirmation_id={confirmation_id}'
+        )
+        await cls.client.send_message(
+            message
+        )
